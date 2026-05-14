72bb8e7a0eba523e7a95565a2a87a0c58cff1b82
Rafeixons <goncalvesrafeixons@gmail.com>
2026-05-13 21:55:19 -0300

    Criação do projeto Calculadora Funcional e seu designer

--- src/exemplojavaswing/CalculadoraFuncional.java
+++ src/exemplojavaswing/CalculadoraFuncional.java
@@ -28,30 +28,145 @@ public class CalculadoraFuncional extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
+        jButton6 = new javax.swing.JButton();
+        jButton7 = new javax.swing.JButton();
+        jButton8 = new javax.swing.JButton();
+        jButton9 = new javax.swing.JButton();
+        jb_soma = new javax.swing.JButton();
+        jb_subtracao = new javax.swing.JButton();
+        jb_divisão = new javax.swing.JButton();
+        txf_resultado = new javax.swing.JTextField();
+        jb_multiplicacao = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jButton1.setText("jButton1");
+        jPanel1.setBackground(new java.awt.Color(153, 204, 255));
+        jPanel1.setForeground(new java.awt.Color(255, 255, 255));
+
+        jButton1.setBackground(new java.awt.Color(153, 204, 255));
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/num_1.png"))); // NOI18N
+
+        jButton2.setBackground(new java.awt.Color(153, 204, 255));
+        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/num_2.png"))); // NOI18N
+
+        jButton3.setBackground(new java.awt.Color(153, 204, 255));
+        jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/num_3.png"))); // NOI18N
+
+        jButton4.setBackground(new java.awt.Color(153, 204, 255));
+        jButton4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/num_8.png"))); // NOI18N
+
+        jButton5.setBackground(new java.awt.Color(153, 204, 255));
+        jButton5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/num_4.png"))); // NOI18N
+
+        jButton6.setBackground(new java.awt.Color(153, 204, 255));
+        jButton6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/num_5.png"))); // NOI18N
+
+        jButton7.setBackground(new java.awt.Color(153, 204, 255));
+        jButton7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/num_6.png"))); // NOI18N
+
+        jButton8.setBackground(new java.awt.Color(153, 204, 255));
+        jButton8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/num_7.png"))); // NOI18N
+
+        jButton9.setBackground(new java.awt.Color(153, 204, 255));
+        jButton9.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/num_9.png"))); // NOI18N
+
+        jb_soma.setBackground(new java.awt.Color(255, 204, 153));
+        jb_soma.setFont(new java.awt.Font("Lucida Fax", 1, 18)); // NOI18N
+        jb_soma.setForeground(new java.awt.Color(255, 255, 255));
+        jb_soma.setText("+");
+
+        jb_subtracao.setBackground(new java.awt.Color(255, 204, 153));
+        jb_subtracao.setFont(new java.awt.Font("Lucida Fax", 1, 18)); // NOI18N
+        jb_subtracao.setForeground(new java.awt.Color(255, 255, 255));
+        jb_subtracao.setText("-");
+
+        jb_divisão.setBackground(new java.awt.Color(255, 204, 153));
+        jb_divisão.setFont(new java.awt.Font("Lucida Fax", 1, 18)); // NOI18N
+        jb_divisão.setForeground(new java.awt.Color(255, 255, 255));
+        jb_divisão.setText("/");
+
+        txf_resultado.setEditable(false);
+        txf_resultado.setBackground(new java.awt.Color(255, 204, 153));
+        txf_resultado.setFont(new java.awt.Font("Lucida Fax", 1, 14)); // NOI18N
+        txf_resultado.setForeground(new java.awt.Color(0, 0, 0));
+
+        jb_multiplicacao.setBackground(new java.awt.Color(255, 204, 153));
+        jb_multiplicacao.setFont(new java.awt.Font("Lucida Fax", 1, 18)); // NOI18N
+        jb_multiplicacao.setForeground(new java.awt.Color(255, 255, 255));
+        jb_multiplicacao.setText("*");
+        jb_multiplicacao.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(27, 27, 27)
-                .addComponent(jButton1)
-                .addContainerGap(294, Short.MAX_VALUE))
+                .addGap(33, 33, 33)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jb_divisão)
+                        .addGap(39, 39, 39)
+                        .addComponent(jb_subtracao)
+                        .addGap(44, 44, 44)
+                        .addComponent(jb_soma)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jb_multiplicacao))
+                    .addComponent(txf_resultado, javax.swing.GroupLayout.PREFERRED_SIZE, 258, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jButton1)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton2)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton3))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jButton5)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton6)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton7))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jButton8)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton4)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton9)))
+                .addContainerGap(49, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(161, 161, 161)
-                .addComponent(jButton1)
-                .addContainerGap(372, Short.MAX_VALUE))
+                .addGap(21, 21, 21)
+                .addComponent(txf_resultado, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(34, 34, 34)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jb_soma)
+                    .addComponent(jb_subtracao)
+                    .addComponent(jb_divisão)
+                    .addComponent(jb_multiplicacao))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(jButton2)
+                    .addComponent(jButton3))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton5)
+                    .addComponent(jButton6)
+                    .addComponent(jButton7))
+                .addGap(32, 32, 32)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton8)
+                    .addComponent(jButton4)
+                    .addComponent(jButton9))
+                .addContainerGap(68, Short.MAX_VALUE))
         );
 
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 400, 560));
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, -10, 340, 550));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -93,6 +208,19 @@ public class CalculadoraFuncional extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
+    private javax.swing.JButton jButton6;
+    private javax.swing.JButton jButton7;
+    private javax.swing.JButton jButton8;
+    private javax.swing.JButton jButton9;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JButton jb_divisão;
+    private javax.swing.JButton jb_multiplicacao;
+    private javax.swing.JButton jb_soma;
+    private javax.swing.JButton jb_subtracao;
+    private javax.swing.JTextField txf_resultado;
     // End of variables declaration//GEN-END:variables
 }
